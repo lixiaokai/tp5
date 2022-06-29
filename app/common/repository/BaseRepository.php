6d0696781c35ffcbece8f40cea3e7adf0991fcb2
李小凯 <lixiaokai@live.com>
2022-06-29 18:03:59 +0800

    完善：仓库基类

--- app/common/repository/BaseRepository.php
+++ app/common/repository/BaseRepository.php
@@ -2,43 +2,80 @@
 
 namespace app\common\repository;
 
-use InvalidArgumentException;
-use think\db\exception\DataNotFoundException;
-use think\db\exception\ModelNotFoundException;
+use app\common\exception\DataSaveErrorException;
+use app\common\exception\DataNotFoundException;
 use think\db\Query;
 use think\exception\DbException;
+use think\Loader;
 use think\Model;
 
+/**
+ * 仓库基类.
+ */
 abstract class BaseRepository
 {
     /**
      * @var string
      */
-    protected $model;
+    protected $modelClass;
+
+    /**
+     * @var Model
+     */
+    public $model;
 
     /**
      * @var Query
      */
-    protected $query;
+    public $query;
 
     public function __construct()
     {
-        /**
-         * @var Model $model
-         */
-        $model = (new $this->model);
-        if (! $model instanceof Model) {
-            throw new InvalidArgumentException('property not exists:' . __CLASS__ . ' -> $model');
-        }
-
-        $this->query = $model->getQuery();
+        $this->model = Loader::model($this->modelClass);
+        $this->query = $this->model->getQuery();
     }
 
     /**
-     * @throws DbException|DataNotFoundException|ModelNotFoundException
+     * 获取 1 行数据 - 根据 ID.
+     *
+     * @throws DataNotFoundException
      */
-    public function getById(int $id)
+    public function getById(int $id): Model
     {
-        return $this->query->findOrFail($id);
+        $message = 'table data not Found';
+
+        try {
+            $row = $this->model::get($id);
+            if (! $row) {
+                throw new DataNotFoundException($message);
+            }
+            return $row;
+        } catch (DbException $e) {
+            throw new DataNotFoundException($message);
+        }
+    }
+
+    /**
+     * @throws DataSaveErrorException
+     */
+    public function create(array $data): Model
+    {
+        try {
+            return $this->model::create($data);
+        } catch (\Exception $e) {
+            throw new DataSaveErrorException();
+        }
+    }
+
+    /**
+     * @throws DataSaveErrorException
+     */
+    public function update(Model $model, array $data): Model
+    {
+        if ($model->save($data) === false) {
+            throw new DataSaveErrorException();
+        }
+
+        return $model;
     }
 }
