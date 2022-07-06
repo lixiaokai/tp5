b4e6e988a38e2eb1a9dc4b32c18a8656875c69ec
李小凯 <lixiaokai@live.com>
2022-07-06 17:43:59 +0800

    增加模型基类

---
+++ app/common/model/BaseModel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\common\model;
+
+use think\db\Query;
+use think\Model;
+
+/**
+ * 模型基类.
+ *
+ * @method static Query where(mixed $field, string $op = null, mixed $condition = null) 查询条件
+ */
+class BaseModel extends Model
+{
+
+}
